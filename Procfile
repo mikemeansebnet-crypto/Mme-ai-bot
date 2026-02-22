26e5c800009a0f94ca26f002c7003664f5eb2b80
Chris Ebnet <cebnet1@gmail.com>
2026-02-21 21:32:51 -0600

    modifying code for performance to handle multiple calls.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn main:app
+web: gunicorn --workers 3 --threads 8 --bind 0.0.0.0:$PORT main:app
--- app.py
+++ app.py
@@ -1,49 +1,27 @@
- 21544from flask import Flask, request, jsonify
-
-
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
-
 
 @app.route("/")
 
 def home():
-
     return "MME AI Bot is running"
-
-
 
 @app.route("/estimate", methods=["POST"])
 
 def estimate():
 
     data = request.json
-
     service = data.get("service")
-
     details = data.get("details")
-
-
-
     # Placeholder pricing logic
-
     price_range = "$100 - $300"
 
-
-
     return jsonify({
-
         "service": service,
-
         "estimated_range": price_range,
-
         "message": "This is a rough estimate. Final pricing requires inspection."
-
     })
 
-
-
 if __name__ == "__main__":
-
     app.run(host="0.0.0.0", port=3
