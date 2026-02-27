2082ed7a3e9fcdd84752de9463975eb9b9ed7bdb
mikemeansebnet-crypto <mikemeansebnet@gmail.com>
2026-02-27 12:54:21 -0500

    refactor: move redis configuration to app/config.py

--- app/app/config.py
+++ app/app/config.py
@@ -1,1 +1,8 @@
+import os
+import redis
 
+REDIS_URL = os.getenv("REDIS_URL")
+REDIS_PREFIX = os.getenv("REDIS_PREFIX", "mmeai:call:")
+REDIS_TTL_SECONDS = int(os.getenv("REDIS_TTL_SECONDS", "7200"))
+
+redis_client = redis.from_url(REDIS_URL, decode_responses=True) if REDIS_URL else None
